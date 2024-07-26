8d5ecba7ff0b39d955030ddccfef0d4bdb5832af
brnocorreia <bruninhocorreia2013@gmail.com>
2024-07-26 00:56:48 -0300

    feat: first version

---
+++ entity/player.go
@@ -0,0 +1,30 @@
+package entity
+
+import "fmt"
+
+type Player struct {
+	Id     int
+	Power  int
+	Points int
+}
+
+func NewPlayer(id int, power int) *Player {
+	return &Player{
+		Id:     id,
+		Power:  power,
+		Points: 0,
+	}
+}
+
+func (p *Player) GetDamage() int {
+	return p.Power
+}
+
+func (p *Player) GetPoints() int {
+	return p.Points
+}
+
+func (p *Player) AddPoint() {
+	p.Points++
+	fmt.Printf("O player %d tem %d pontos\n", p.Id, p.Points)
+}
